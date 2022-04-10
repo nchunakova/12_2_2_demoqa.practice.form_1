718cda1f369fa267efd5b57f2b3b8fe1319ed429
Natalia Chunakova <n-chunakova@yandex.ru>
2022-04-10 13:41:34 +0400

    added secectors till calendar

--- src/test/java/qa/nchunakova/FillFormTests.java
+++ src/test/java/qa/nchunakova/FillFormTests.java
@@ -3,7 +3,8 @@ package qa.nchunakova;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FillFormTests {
 
@@ -16,6 +17,22 @@ public class FillFormTests {
 
     @Test
     void fillFormTest(){
+        String firstName = "Sia";
+        String lastName = "Bamberg";
+        String userEmail = "sia@bamberg.com";
+        String gender = "Female";
+        String userNumber = "0123456789";
+
         open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").click();
+        $("#submit").click();
     }
 }
